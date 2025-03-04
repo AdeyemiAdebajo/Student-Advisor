76abf83854f0a7fe56727ada773ee22104b0d7fe
Adeyemi <adeyemi.adebajo@canadorestudents.ca>
2025-03-04 03:10:55 -0500

    Database updated

---
+++ Pages/Student/Index.cshtml.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using StudentAdvisor.Data;
+
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentAdvisor.Pages.Student
+{
+    public class IndexModel : PageModel
+    {
+        
+    }
+}
+// public void OnGet()
+//         {
+//         }
