950b9f2462e30f0669866b0451a4606aa0e961fc
Adeyemi Adebajo <127756450+AdeyemiAdebajo@users.noreply.github.com>
2025-07-20 23:28:18 -0400

    readme

---
+++ Data/RoleInitializer.cs
@@ -0,0 +1,23 @@
+using System;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace StudentAdvisor.Data;
+
+ public static class RoleInitializer
+    {
+        public static async Task SeedRoles(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            string[] roleNames = { "Viewer", "Admin" };
+
+            foreach (var roleName in roleNames)
+            {
+                if (!await roleManager.RoleExistsAsync(roleName))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(roleName));
+                }
+            }
+        }
+    }
