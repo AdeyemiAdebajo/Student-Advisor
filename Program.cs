134be80e4007104bdd92e54287cf3f89661106ee
Adeyemi Adebajo <127756450+AdeyemiAdebajo@users.noreply.github.com>
2025-07-21 23:25:50 -0400

    .env

--- Program.cs
+++ Program.cs
@@ -4,8 +4,10 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using StudentAdvisor.Models;
 using StudentAdvisor.Data;
 using Microsoft.Extensions.DependencyInjection;
+using DotNetEnv;
 
 var builder = WebApplication.CreateBuilder(args);
+DotNetEnv.Env.Load();
 
 // Add services to the container with authentication enforced
 // builder.Services.AddRazorPages();
@@ -20,23 +22,35 @@ builder.Services.AddRazorPages(options =>
     options.Conventions.AllowAnonymousToPage("/Identity/Account/Login");
 });
 
+
 // builder.Services.AddDbContext<AppDbcontext2>(options =>
 //     options.UseSqlServer(builder.Configuration.GetConnectionString("StudentAdvisorDb") ?? throw new InvalidOperationException("Connection string 'AppDbcontext2' not found.")));
 
 
 ServerVersion serverVersion = new MariaDbServerVersion(new Version(10, 4, 32));
+string connStr = Environment.GetEnvironmentVariable("StudentAdvisor_Dbserver");
 
+// builder.Services.AddDbContext<AppDbcontext>(options =>
+//     options.UseMySql(builder.Configuration.GetConnectionString("StudentAdvisorDb"), serverVersion)
+//     .LogTo(Console.WriteLine, LogLevel.Information)
+//     .EnableSensitiveDataLogging()
+//     .EnableDetailedErrors()
+// );
 builder.Services.AddDbContext<AppDbcontext>(options =>
-    options.UseMySql(builder.Configuration.GetConnectionString("StudentAdvisorDb"), serverVersion)
-    .LogTo(Console.WriteLine, LogLevel.Information)
-    .EnableSensitiveDataLogging()
-    .EnableDetailedErrors()
+    options.UseMySql(connStr, serverVersion)
+           .LogTo(Console.WriteLine, LogLevel.Information)
+           //Use the following line to enable sensitive data logging
 );
+// builder.Services.AddDbContext<AppDbcontext2>(options =>
+//     options.UseMySql(builder.Configuration.GetConnectionString("StudentAdvisorDb"), serverVersion)
+//     .LogTo(Console.WriteLine, LogLevel.Information)
+//     .EnableSensitiveDataLogging()
+//     .EnableDetailedErrors()
+// );
 builder.Services.AddDbContext<AppDbcontext2>(options =>
-    options.UseMySql(builder.Configuration.GetConnectionString("StudentAdvisorDb"), serverVersion)
-    .LogTo(Console.WriteLine, LogLevel.Information)
-    .EnableSensitiveDataLogging()
-    .EnableDetailedErrors()
+    options.UseMySql(connStr, serverVersion)
+           .LogTo(Console.WriteLine, LogLevel.Information)
+           //Use the following line to enable sensitive data logging
 );
 
 builder.Services.AddDefaultIdentity<Register>(options =>
